ee3df76b32814239d7389a72ee3632f3518e8bd1
leeseungjoo98 <pool9789@naver.com>
2020-04-20 15:30:03 +0900

    namespace
    namespace는 대규모 프로젝트에서 매우 중요하다 . 왜냐하면 이름충돌 문제가 발생할 수 있기 때문이다.
    +) 범위 지정자 ::

---
+++ namespace.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+namespace BestComImpl
+{
+	void SimpleFunc() {
+		std::cout << "BestCom이 정의하는 함수" << std::endl;
+
+	}
+}
+namespace ProgComImpl
+{
+	void SimpleFunc() {
+		std::cout << "ProgComImpl이 정의하는 함수" << std::endl;
+
+	}
+}
+int main() {
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	return 0;
+}
