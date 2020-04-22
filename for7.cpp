e269176f360e1f3498a374b0bdc3079f4f48f6ed
leeseungjoo98 <pool9789@naver.com>
2020-04-23 01:56:13 +0900

    이중 for문
    원하는 숫자를 입력하면 직각삼각형이 나오게 ..
    이중 for문 연습 중

---
+++ for7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int number;
+	cout << "원하는 숫자:";
+	cin >> number;
+	for (int i = 0; i < number; i++) {
+		for (int g = 2+number; g > i;g--) {
+			cout << " ";
+			
+		}
+		for (int h = 0; h < i + 1;h++) {
+			cout << "*";
+
+		}
+		cout << endl;
+		
+	}
+
+	return 0;
+}
