2ff4f25d3fa12e2303f239bf1f3eb6a322600ee3
leeseungjoo98 <pool9789@naver.com>
2020-04-23 11:17:00 +0900

    학생성적 검색 program
    배열을 포인터에 넣기

---
+++ array-score2.cpp
@@ -0,0 +1,26 @@
+///학생 성적검색 program ver 포인터 배열
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main() {
+	int amount;
+	cout << "총 인원 :";
+	cin >> amount;
+	int *score = new int[amount];
+	int number;
+	for (int j = 0; j < amount; j++) {
+		cout << "학생" << j + 1 << "의 성적 :";
+		cin >> score[j];
+		
+}
+	cout << "학생출석번호: ";
+	cin >> number;
+	int *g = score;
+	cout << "학생" << number + 1 << "번:" << score[number];
+	cout << "학생" << number + 1 << "번:" << g[number];
+	
+
+	return 0;
+}
