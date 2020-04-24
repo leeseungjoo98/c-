ceddbcd84bf6ebc2616f2496ce0690c912361809
leeseungjoo98 <pool9789@naver.com>
2020-04-24 13:44:43 +0900

    문자열 매개변수 사용하기
    문자열을 쓰기전 메모리 할당을 해주어야 한다. 그래서 난 동적할당을 썼다. 그리고 문자열 매개변수 쓰는 방법을 자연스럽게 사용할 수 있어야 한다.

---
+++ string_array1.cpp
@@ -0,0 +1,29 @@
+//////////문자열 매개변수 사용하기
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+void printout(char print1[], char print2[]);
+int main() {
+	char *sentence1 = new char[156];
+	char *sentence2 = new char[156];
+	cout << "문자열 입력 :";
+	cin >> sentence1;
+	cout << "문자열 입력 :";
+	cin >> sentence2;
+	printout(sentence1, sentence2);
+	
+	return 0;
+}
+void printout(char print1[], char print2[]) {
+
+	if (strcmp(print1, print2) == 0)
+	{
+		cout << "같음";
+
+	}
+	else
+		cout << "다름";
+
+
+}
