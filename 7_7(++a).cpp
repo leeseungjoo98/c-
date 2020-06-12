55bb75b84cf0243cb145851857f1ad15f44d9236
leeseungjoo98 <pool9789@naver.com>
2020-06-12 11:05:52 +0900

    전위 연산자 작성

---
+++ 7_7(++a).cpp
@@ -0,0 +1,27 @@
+#include <string>
+using namespace std;
+class Power {
+	int kick;
+	int punch;
+public:
+	Power(int kick = 0, int punch = 0) {
+		this->kick = kick, this->punch = punch;
+	}
+	void show();
+	Power operator ++ ();
+};
+void Power::show() {
+	cout << "kick= " << kick << ',' << "punch= " << punch << endl;
+}
+Power Power::operator++() {
+	kick++;
+	punch++;
+	return *this;
+}
+int main() {
+	Power a(3, 5), b;
+	b = ++a;
+	a.show();
+	b.show();
+	return 0;
+}
